ce5e79e6bcb6e1177735713e2c71a777264066dd
Victoria <vikshab2010@hotmail.com>
2016-07-27 10:46:23 -0700

    Added values to values.js, added those values to prompt.js and updated manifest file to display correctly new data

--- js/prompt.js
+++ js/prompt.js
@@ -1,2 +1,4 @@
 var userName = prompt("Hello, what is you name?")
-document.body.innerHTML = "<h1>Hello, " + userName + "!</h1>";
+document.body.innerHTML = "<h1>Hello, " + userName + "!</h1>" +
+                          "<p>" + projectName + " " + versionNumber +
+                          " accessed on: " + currentTime + "</p>";
