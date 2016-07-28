00564ea139e7fb808fdca178ec78de1ab1b3804f
Victoria <vikshab2010@hotmail.com>
2016-07-27 21:47:33 -0700

    Changed prompt file to add prompt for phone number and validation

--- js/prompt.js
+++ js/prompt.js
@@ -1,4 +1,14 @@
 var userName = prompt("Hello, what's you name?");
-document.body.innerHTML = "<h1>Hello, " + userName + "!</h1>" +
-                          "<p>" + projectName + " " + versionNumber +
-                          " accessed on: " + currentTime + "</p>";
+var phoneNumber = prompt("Hello " + userName + " ,what's your phone number?");
+var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+var output = "<h1>Hello, " + userName + "!</h1>";
+
+// Is the phone number valid?
+if(phoneNumberPattern.test(phoneNumber)){
+  output = output "<p>" + pbValues.projectName + " " + pbValues.versionNumber +
+          " viewed on: " + pbValues.currentTime + "</p>";
+} else {
+  output = output + "<h2>That phone number is invalid: " + phoneNumber;
+}
+
+document.body.innerHTML = output;
--- js/values.js
+++ js/values.js
@@ -1,9 +1,19 @@
-var projectName = "puppybook";
-var versionNumber = "0.0.1";
+// var projectName = "puppybook";
+// var versionNumber = "0.0.1";
 var currentDate = new Date();
-var currentTime = currentDate.getFullYear() + "-" +
-                  (currentDate.getMonth() + 1) + "-" +
-                  currentDate.getDate() + " at " +
-                  currentDate.getHours() + ":" +
-                  currentDate.getMinutes() + ":" +
-                  currentDate.getSeconds();
+// var currentTime = currentDate.getFullYear() + "-" +
+//                   (currentDate.getMonth() + 1) + "-" +
+//                   currentDate.getDate() + " at " +
+//                   currentDate.getHours() + ":" +
+//                   currentDate.getMinutes() + ":" +
+//                   currentDate.getSeconds();
+var pbValues = {
+  projectName: "puppybook",
+  versionNumber: "0.0.1",
+  currentTime: currentDate.getFullYear() + "-" +
+                    (currentDate.getMonth() + 1) + "-" +
+                    currentDate.getDate() + " at " +
+                    currentDate.getHours() + ":" +
+                    currentDate.getMinutes() + ":" +
+                    currentDate.getSeconds()
+};
