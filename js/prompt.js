7500cf914a2211f3537778fbc0cfac96d00cca58
Victoria <vikshab2010@hotmail.com>
2016-07-29 20:16:33 -0700

    Changed phoneNumberPattern to use group in regex

--- js/prompt.js
+++ js/prompt.js
@@ -1,11 +1,11 @@
 var userName = prompt("Hello, what's you name?");
 var phoneNumber = prompt("Hello " + userName + " ,what's your phone number?");
-var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+var phoneNumberPattern = /(?:1-)?\(?\d{3}[\-\)]\d{3}-\d{4}/;
 var output = "<h1>Hello, " + userName + "!</h1>";
 
 // Is the phone number valid?
 if(phoneNumberPattern.test(phoneNumber)){
-  output = output "<p>" + pbValues.projectName + " " + pbValues.versionNumber +
+  output = output + "<p>" + pbValues.projectName + " " + pbValues.versionNumber +
           " viewed on: " + pbValues.currentTime + "</p>";
 } else {
   output = output + "<h2>That phone number is invalid: " + phoneNumber;
--- release/main.js
+++ release/main.js
@@ -1,14 +1,34 @@
-var projectName = "puppybook";
-var versionNumber = "0.0.1";
+// var projectName = "puppybook";
+// var versionNumber = "0.0.1";
 var currentDate = new Date();
-var currentTime = currentDate.getFullYear() + "-" +
-                  (currentDate.getMonth() + 1) + "-" +
-                  currentDate.getDate() + " at " +
-                  currentDate.getHours() + ":" +
-                  currentDate.getMinutes() + ":" +
-                  currentDate.getSeconds();
+// var currentTime = currentDate.getFullYear() + "-" +
+//                   (currentDate.getMonth() + 1) + "-" +
+//                   currentDate.getDate() + " at " +
+//                   currentDate.getHours() + ":" +
+//                   currentDate.getMinutes() + ":" +
+//                   currentDate.getSeconds();
+var pbValues = {
+  projectName: "puppybook",
+  versionNumber: "0.0.1",
+  currentTime: currentDate.getFullYear() + "-" +
+                    (currentDate.getMonth() + 1) + "-" +
+                    currentDate.getDate() + " at " +
+                    currentDate.getHours() + ":" +
+                    currentDate.getMinutes() + ":" +
+                    currentDate.getSeconds()
+};
 
 var userName = prompt("Hello, what's you name?");
-document.body.innerHTML = "<h1>Hello, " + userName + "!</h1>" +
-                          "<p>" + projectName + " " + versionNumber +
-                          " accessed on: " + currentTime + "</p>";
+var phoneNumber = prompt("Hello " + userName + " ,what's your phone number?");
+var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+var output = "<h1>Hello, " + userName + "!</h1>";
+
+// Is the phone number valid?
+if(phoneNumberPattern.test(phoneNumber)){
+  output = output + "<p>" + pbValues.projectName + " " + pbValues.versionNumber +
+          " viewed on: " + pbValues.currentTime + "</p>";
+} else {
+  output = output + "<h2>That phone number is invalid: " + phoneNumber;
+}
+
+document.body.innerHTML = output;
