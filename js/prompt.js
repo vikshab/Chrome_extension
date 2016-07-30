81c034efe6e0bf9c6d2f9eeac6bd764adc6620b8
Victoria <vikshab2010@hotmail.com>
2016-07-29 20:21:02 -0700

    Added a capture to regex to access area code, added areaCode object to pbValues

--- js/prompt.js
+++ js/prompt.js
@@ -1,6 +1,6 @@
 var userName = prompt("Hello, what's you name?");
 var phoneNumber = prompt("Hello " + userName + " ,what's your phone number?");
-var phoneNumberPattern = /(?:1-)?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+var phoneNumberPattern = /(?:1-)?\(?(\d{3})[\-\)]\d{3}-\d{4}/;
 var output = "<h1>Hello, " + userName + "!</h1>";
 
 // Is the phone number valid?
--- js/values.js
+++ js/values.js
@@ -15,5 +15,13 @@ var pbValues = {
                     currentDate.getDate() + " at " +
                     currentDate.getHours() + ":" +
                     currentDate.getMinutes() + ":" +
-                    currentDate.getSeconds()
+                    currentDate.getSeconds();
+  areaCode: {
+    "408" : "Silicon Valley",
+    "702" : "Las Vegas",
+    "801" : "Northern Utah",
+    "765" : "West Lafayette",
+    "901" : "Memphis",
+    "507" : "Rochester, MN"
+  }
 };
