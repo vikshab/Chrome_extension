0726961ce157b64a2c4c91a9507949eb058bb26d
Victoria <vikshab2010@hotmail.com>
2016-07-29 20:21:33 -0700

    Deleted commented out code

--- js/values.js
+++ js/values.js
@@ -1,12 +1,4 @@
-// var projectName = "puppybook";
-// var versionNumber = "0.0.1";
 var currentDate = new Date();
-// var currentTime = currentDate.getFullYear() + "-" +
-//                   (currentDate.getMonth() + 1) + "-" +
-//                   currentDate.getDate() + " at " +
-//                   currentDate.getHours() + ":" +
-//                   currentDate.getMinutes() + ":" +
-//                   currentDate.getSeconds();
 var pbValues = {
   projectName: "puppybook",
   versionNumber: "0.0.1",
